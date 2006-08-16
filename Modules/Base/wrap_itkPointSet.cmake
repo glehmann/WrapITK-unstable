56ecb1b5fa0146304ea46054437a9f379f042917
Zachary Pincus <zpincus@stanford.edu>
2006-08-16 23:21:08 +0200

    Changes to PointSet and MeshTraits classes for consistency with kernel transforms.
    darcs-hash:20060816212108-733a7-7219476d52d3069304286530dca343ce0835bfcc.gz

--- Modules/Base/wrap_itkPointSet.cmake
+++ Modules/Base/wrap_itkPointSet.cmake
@@ -4,8 +4,13 @@ WRAP_INCLUDE("itkDefaultDynamicMeshTraits.h")
 WRAP_CLASS("itk::PointSet" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
     WRAP_TEMPLATE("${ITKM_D}${d}S"
-      "${ITKT_D},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D} >")
+      "${ITKT_D},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D},${ITKT_D} >")
     WRAP_TEMPLATE("${ITKM_D}${d}D"
-      "${ITKT_D},${d},itk::DefaultDynamicMeshTraits< ${ITKT_D},${d},${d},${ITKT_D} >")
+      "${ITKT_D},${d},itk::DefaultDynamicMeshTraits< ${ITKT_D},${d},${d},${ITKT_D},${ITKT_D} >")
+# Also wrap the point set type needed for the kernel transforms,
+# which may be a bug in ITK but they're needed currently.
+    WRAP_TEMPLATE("${ITKM_PD${d}}${d}S"
+      "${ITKT_PD${d}},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D},${ITKT_D} >")
+
   ENDFOREACH(d)
 END_WRAP_CLASS()
