fa9ab9c9aa95c70f61e85a6085c95b6ea53841cf
Zachary Pincus <zpincus@stanford.edu>
2006-08-17 02:49:09 +0200

    More fixes for PointSet/MeshTraits for KernelTransforms
    darcs-hash:20060817004909-733a7-48f40536923b438aa15e37c0802cafbc1b82cd21.gz

--- Modules/Base/wrap_itkDefaultDynamicMeshTraits.cmake
+++ Modules/Base/wrap_itkDefaultDynamicMeshTraits.cmake
@@ -1,10 +1,8 @@
 WRAP_CLASS("itk::DefaultDynamicMeshTraits")
   FOREACH(d ${WRAP_ITK_DIMS})
-    WRAP_TEMPLATE("${ITKM_D}${d}${d}${ITKM_D}" "${ITKT_D},${d},${d},${ITKT_D}")
+  WRAP_TEMPLATE("${ITKM_D}${d}${d}${ITKM_D}${ITKM_D}" "${ITKT_D},${d},${d},${ITKT_D},${ITKT_D}")
+  WRAP_TEMPLATE("${ITKM_D}${d}${d}${ITKM_D}" "${ITKT_D},${d},${d},${ITKT_D}")
   ENDFOREACH(d)  
 
-#  FOREACH(d ${WRAP_ITK_DIMS})
-#    WRAP_TEMPLATE("${ITKM_PD${d}}${d}${d}${ITKM_D}${ITKM_D}${ITKM_PD${d}}" "${ITKT_PD${d}},${d},${d},${ITKT_D},${ITKT_D},${ITKT_PD${d}}")
-#  ENDFOREACH(d)  
 
 END_WRAP_CLASS()
--- Modules/Base/wrap_itkPointSet.cmake
+++ Modules/Base/wrap_itkPointSet.cmake
@@ -4,9 +4,9 @@ WRAP_INCLUDE("itkDefaultDynamicMeshTraits.h")
 WRAP_CLASS("itk::PointSet" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
     WRAP_TEMPLATE("${ITKM_D}${d}S"
-      "${ITKT_D},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D},${ITKT_D} >")
+      "${ITKT_D},${d},itk::DefaultStaticMeshTraits< ${ITKT_D},${d},${d},${ITKT_D} >")
     WRAP_TEMPLATE("${ITKM_D}${d}D"
-      "${ITKT_D},${d},itk::DefaultDynamicMeshTraits< ${ITKT_D},${d},${d},${ITKT_D},${ITKT_D} >")
+      "${ITKT_D},${d},itk::DefaultDynamicMeshTraits< ${ITKT_D},${d},${d},${ITKT_D} >")
 # Also wrap the point set type needed for the kernel transforms,
 # which may be a bug in ITK but they're needed currently.
     WRAP_TEMPLATE("${ITKM_PD${d}}${d}S"
