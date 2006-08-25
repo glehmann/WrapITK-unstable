a8ff7f3a024532701f7bbf857bdbaa338b809708
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-08-25 13:40:21 +0200

    move vtk viewer to ItkVtkGlue external project
    darcs-hash:20060825114021-2fc9d-f775a39786242b51e9a60528cc250c338c0cf7f0.gz

--- Python/itkExtras/__init__.py
+++ Python/itkExtras/__init__.py
@@ -137,9 +137,10 @@ def write(imageOrFilter, fileName):
   
 # choose the method to call according to the dimension of the image
 def show(input, **kargs) :
+    import itk
     img = image(input)
-    if img.GetImageDimension() == 3:
-	return show3D(input, **kargs)
+    if img.GetImageDimension() == 3 and "show3D" in dir(itk):
+	return itk.show3D(input, **kargs)
     else :
 	# print "2D not supported yet, use the 3D viewer."
 	return show2D(input, **kargs)
@@ -155,166 +156,6 @@ class show2D :
     import os
     os.system("imview %s -fork" % self.__tmpFile__.name)
     #tmpFile.close()
-
-# display scalar images in volume
-class show3D :
-  def __init__(self, input=None, MinOpacity=0.0, MaxOpacity=0.1) :
-    import qt
-    import vtk
-    from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-    self.__MinOpacity__ = MinOpacity
-    self.__MaxOpacity__ = MaxOpacity
-    # every QT app needs an app
-    self.__app__ = qt.QApplication(['itkviewer'])
-    # create the widget
-    self.__widget__ = QVTKRenderWindowInteractor()
-    self.__ren__ = vtk.vtkRenderer()
-    self.__widget__.GetRenderWindow().AddRenderer(self.__ren__)
-    self.__itkvtkConverter__ = None
-    self.__volumeMapper__ = vtk.vtkVolumeTextureMapper2D()
-    self.__volume__ = vtk.vtkVolume()
-    self.__volumeProperty__ = vtk.vtkVolumeProperty()
-    self.__volume__.SetMapper(self.__volumeMapper__)
-    self.__volume__.SetProperty(self.__volumeProperty__)
-    self.__ren__.AddVolume(self.__volume__)
-    self.__outline__ = None
-    self.__outlineMapper__ = None
-    self.__outlineActor__ = None
-    self.AdaptColorAndOpacity(0, 255)
-    if input :
-      self.SetInput(input)
-      self.AdaptColorAndOpacity()
-      
-  def Render(self):
-    self.__ren__.Render()
-    
-  def GetWidget(self) :
-    return self.__widget__
-  
-  def GetRenderer(self) :
-    return self.__ren__
-  
-  def GetConverter(self) :
-    return self.__itkvtkConverter__
-  
-  def GetVolumeMapper(self) :
-    return self.__volumeMapper__
-  
-  def GetVolume(self) :
-    return self.__volume__
-  
-  def GetVolumeProperty(self) :
-    return self.__volumeProperty__
-  
-  def Show(self) :
-    self.__widget__.show()
-    
-  def Hide(self) :
-    self.__widget__.hide()
-    
-  def SetInput(self, input) :
-    from __builtin__ import range # required because range is overladed in this module
-    img = image(input)
-    self.__input__ = img
-    if img :
-      # Update to try to avoid to exit if a c++ exception is throwed
-      # sadely, it will not prevent the program to exit later...
-      # a real fix would be to wrap c++ exception in vtk
-      img.UpdateOutputInformation()
-      img.Update()
-      import itk
-      # flip the image to get the same representation than the vtk one
-      self.__flipper__ = itk.FlipImageFilter[img].New(Input=img)
-      axes = self.__flipper__.GetFlipAxes()
-      axes.SetElement(1, True)
-      self.__flipper__.SetFlipAxes(axes)
-      # change the spacing while still keeping the ratio to workaround vtk bug
-      # when spacing is very small
-      spacing_ = spacing(img)
-      normSpacing = []
-      for i in range(0, spacing_.Size()):
-        normSpacing.append( spacing_.GetElement(i) / spacing_.GetElement(0) )
-      self.__changeInfo__ = itk.ChangeInformationImageFilter[img].New(self.__flipper__, OutputSpacing=normSpacing, ChangeSpacing=True)
-      # now really convert the data
-      self.__itkvtkConverter__ = itk.ImageToVTKImageFilter[img].New(self.__changeInfo__)
-      self.__volumeMapper__.SetInput(self.__itkvtkConverter__.GetOutput())
-      # needed to avoid warnings
-      # self.__itkvtkConverter__.GetOutput() must be callable
-      import vtk
-      if not self.__outline__ :
-	  self.__outline__ = vtk.vtkOutlineFilter()
-	  self.__outline__.SetInput(self.__itkvtkConverter__.GetOutput())
-	  self.__outlineMapper__ = vtk.vtkPolyDataMapper()
-	  self.__outlineMapper__.SetInput(self.__outline__.GetOutput())
-	  self.__outlineActor__ = vtk.vtkActor()
-	  self.__outlineActor__.SetMapper(self.__outlineMapper__)
-	  self.__ren__.AddActor(self.__outlineActor__)
-      else :
-	  self.__outline__.SetInput(self.__itkvtkConverter__.GetOutput())
-
-    self.Render()
-    
-  def GetInput(self):
-    return self.__input__
-  
-  def AdaptColorAndOpacity(self, minVal=None, maxVal=None):
-    if minVal == None or maxVal == None :
-      m, M = self.GetRange()
-      if minVal == None :
-        minVal = m
-      if maxVal == None :
-        maxVal = M
-    self.AdaptOpacity(minVal, maxVal)
-    self.AdaptColor(minVal, maxVal)
-    
-  def AdaptOpacity(self, minVal=None, maxVal=None) :
-    import vtk
-    if minVal == None or maxVal == None :
-      m, M = self.GetRange()
-      if minVal == None :
-        minVal = m
-      if maxVal == None :
-        maxVal = M
-    opacityTransferFunction = vtk.vtkPiecewiseFunction()
-    opacityTransferFunction.AddPoint(minVal, self.__MinOpacity__)
-    opacityTransferFunction.AddPoint(maxVal, self.__MaxOpacity__)
-    self.__volumeProperty__.SetScalarOpacity(opacityTransferFunction)
-    
-  def AdaptColor(self, minVal=None, maxVal=None):
-    import vtk
-    if minVal == None or maxVal == None :
-      m, M = self.GetRange()
-      if minVal == None :
-        minVal = m
-      if maxVal == None :
-        maxVal = M
-    colorTransferFunction = vtk.vtkColorTransferFunction()
-    colorTransferFunction.AddHSVPoint(minVal, 0.0, 0.0, 0.0)
-    colorTransferFunction.AddHSVPoint((maxVal-minVal)*0.25, 0.66, 1.0, 1.0)
-    colorTransferFunction.AddHSVPoint((maxVal-minVal)*0.5,  0.44, 1.0, 1.0)
-    colorTransferFunction.AddHSVPoint((maxVal-minVal)*0.75, 0.22, 1.0, 1.0)
-    colorTransferFunction.AddHSVPoint(maxVal,               0.0,  1.0, 1.0)
-    self.__volumeProperty__.SetColor(colorTransferFunction)
-    self.Render()
-    
-  def GetRange(self) :
-    conv = self.GetConverter()
-    conv.Update()
-    return conv.GetOutput().GetScalarRange()
-  
-  def GetMaxOpacity(self) :
-    return self.__MaxOpacity__
-  
-  def GetMinOpacity(self) :
-    return self.__MinOpacity__
-  
-  def SetMaxOpacity(self, val) :
-    self.__MaxOpacity__ = val
-    self.AdaptColorAndOpacity()
-    
-  def SetMinOpacity(self, val) :
-    self.__MinOpacity__ = val
-    self.AdaptColorAndOpacity()
 
 
 class pipeline:
