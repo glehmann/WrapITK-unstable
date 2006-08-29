09ad37d3900c7df0344a2a4d1d4c20f847772ea9
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-08-29 17:57:36 +0200

    level set classes are fixed in ITK cvs
    darcs-hash:20060829155736-2fc9d-03101ffec152bd54b330c2964839be86ed8d43b1.gz

--- Modules/LevelSet/wrap_itkSegmentationLevelSetFunction.cmake
+++ Modules/LevelSet/wrap_itkSegmentationLevelSetFunction.cmake
@@ -1,6 +1,4 @@
 WRAP_CLASS("itk::SegmentationLevelSetFunction" POINTER)
-# cableswig segfaults while processing this wrapper
-
-#   WRAP_IMAGE_FILTER_REAL(2)
+  WRAP_IMAGE_FILTER_REAL(2)
 END_WRAP_CLASS()
       
