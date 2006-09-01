daa63e4f8cdfdeb12e9aa265007b49d62f34eff7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-09-01 11:45:05 +0200

    move CovariantVector wrapper outside wrap_itkVector.cmake
    darcs-hash:20060901094505-2fc9d-3d7a79163688b55e8574acff7cbd99ee68f5f218.gz

--- Modules/Base/wrap_itkVector.cmake
+++ Modules/Base/wrap_itkVector.cmake
@@ -1,7 +1,3 @@
 WRAP_CLASS("itk::Vector")
   SET(WRAPPER_TEMPLATES "${itk_Wrap_Vector}")
 END_WRAP_CLASS()
-
-WRAP_CLASS("itk::CovariantVector")
-  SET(WRAPPER_TEMPLATES "${itk_Wrap_CovariantVector}")
-END_WRAP_CLASS()
