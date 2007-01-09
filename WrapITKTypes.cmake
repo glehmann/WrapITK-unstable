7d7ff1c83ac7b4f4f41f465b44a5ad7afd1e2703
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-09 17:44:06 +0100

    add required 6 dimension
    darcs-hash:20070109164406-2fc9d-1387d7c2f6a0f6dd670c9878cfebaef0961616df.gz

--- WrapITKTypes.cmake
+++ WrapITKTypes.cmake
@@ -89,7 +89,7 @@ WRAP_TYPE("itk::FixedArray" "FA")
     MATH(EXPR d2 "${d} * 2")
     SET(dims ${dims} ${d2})
   ENDFOREACH(d)
-  UNIQUE(array_sizes "${dims};1;3;4")
+  UNIQUE(array_sizes "${dims};1;3;4;6")
   # make sure that 1-D FixedArrays are wrapped. Also wrap for each selected
   # image dimension.
   # 3-D FixedArrays are required as superclass of rgb pixels
