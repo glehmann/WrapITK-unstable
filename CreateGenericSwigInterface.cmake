da26429dd92725ff0733c2256a6f4690179de683
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2008-05-23 14:19:43 +0200

    highly reduce the number of targets
    darcs-hash:20080523121943-2fc9d-8a2cc44358c2c5c4bfc64ce9736ec24a71596298.gz

--- CreateGenericSwigInterface.cmake
+++ CreateGenericSwigInterface.cmake
@@ -6,17 +6,78 @@ MACRO(WRAP_LIBRARY_SWIG_INTERFACE library_name)
   SET(SWIG_INTERFACE_MODULE_CONTENT )
   # store the content of the .includes files - a set of #includes for that module
   SET(SWIG_INTERFACE_INCLUDES_CONTENT )
-  # the list of .i files generated for the module
-  SET(SWIG_INTERFACE_FILES )
   # the list of .idx files generated for the module
   SET(SWIG_INTERFACE_IDX_FILES )
-  # That target can be created first, so we can make it depends on the individual group targets
-  # (itkMedianImageFilterSwig for example), and make those individual targets depends on the whole
-  # ${WRAPPER_LIBRARY_NAME}Idx
-  ADD_CUSTOM_TARGET(${WRAPPER_LIBRARY_NAME}Swig)# DEPENDS ${SWIG_INTERFACE_FILES})
+  # build a list of modules to create the ignerator custom command in 
+  # END_WRAP_LIBRARY_SWIG_INTERFACE
+  SET(SWIG_INTERFACE_MODULES )
 ENDMACRO(WRAP_LIBRARY_SWIG_INTERFACE)
 
 MACRO(END_WRAP_LIBRARY_SWIG_INTERFACE)
+
+  # the list of .i files generated for the module
+  SET(SWIG_INTERFACE_FILES )
+
+  FOREACH(module ${SWIG_INTERFACE_MODULES})
+    # create the swig interface
+    SET(interface_file "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/wrap_${module}.i")
+    SET(xml_file "${WRAPPER_LIBRARY_OUTPUT_DIR}/wrap_${module}.xml")
+    SET(includes_file "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${WRAPPER_LIBRARY_NAME}.includes")
+    # prepare the options
+    SET(opts )
+    FOREACH(dep ${WRAPPER_LIBRARY_DEPENDS})
+      SET(opts ${opts} --mdx "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${dep}.mdx")
+      SET(opts ${opts} --take-includes "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${dep}.includes")
+      SET(opts ${opts} --import "${dep}.i")
+    ENDFOREACH(dep)
+    # import the interface files previously defined instead of importing all the files defined
+    # in the module to avoid many warnings when running swig
+    FOREACH(i ${SWIG_INTERFACE_FILES})
+      GET_FILENAME_COMPONENT(bi "${i}" NAME)
+      SET(opts ${opts} --import "${bi}")
+    ENDFOREACH(i)
+  
+  # MESSAGE("${opts}")
+  
+    # configure the test driver, to set the python path to the pygccxml dir
+  #   FIND_PROGRAM(ITK_TEST_DRIVER itkTestDriver)
+  #   SET(PYTHON_PYGCCXML_DRIVER "${ITK_TEST_DRIVER}"
+  #     --add-before-env PYTHONPATH "${WRAP_ITK_CMAKE_DIR}/pygccxml-0.8.2/"
+  #     "${PYTHON_EXECUTABLE}"
+  #   )
+  
+    # prepare dependencies
+    SET(DEPS )
+    FOREACH(dep ${WRAPPER_LIBRARY_DEPENDS})
+      SET(DEPS ${DEPS} ${${dep}IdxFiles} ${${dep}SwigFiles})
+      # be sure that the module is selected by the user
+      IF(NOT "${WRAP_ITK_MODULES}" MATCHES "(^|;)${dep}(;|$)")
+        MESSAGE(SEND_ERROR "${dep} is required by ${WRAPPER_LIBRARY_NAME} module. Please set WRAP_${dep} to ON, or WRAP_${WRAPPER_LIBRARY_NAME} to OFF.")
+      ENDIF(NOT "${WRAP_ITK_MODULES}" MATCHES "(^|;)${dep}(;|$)")
+    ENDFOREACH(dep)
+  
+    ADD_CUSTOM_COMMAND(
+      OUTPUT ${interface_file}
+      COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/igenerator.py
+        ${opts}
+        --mdx ${mdx_file}
+        --take-includes ${includes_file}
+  #       --import ${module_interface_file}
+        --swig-include wrap_${module}_ext.i
+        -w1 -w3 -w51 -w52 -w53 -w54 #--warning-error
+        # --verbose
+        ${xml_file}
+        ${interface_file}
+      DEPENDS ${DEPS} ${xml_file} ${includes_file} ${CMAKE_BINARY_DIR}/igenerator.py ${SWIG_INTERFACE_IDX_FILES} ${SWIG_INTERFACE_FILES}
+    )
+  #   ADD_CUSTOM_TARGET(${module}Swig DEPENDS ${interface_file})
+  #   ADD_DEPENDENCIES(${module}Swig ${WRAPPER_LIBRARY_NAME}Idx)
+  #   ADD_DEPENDENCIES(${WRAPPER_LIBRARY_NAME}Swig ${module}Swig)
+  
+    SET(SWIG_INTERFACE_FILES ${SWIG_INTERFACE_FILES} ${interface_file})
+  ENDFOREACH(module)
+
+
   # the mdx file
   SET(mdx_file "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${WRAPPER_LIBRARY_NAME}.mdx")
   SET(CONFIG_INDEX_FILE_CONTENT "${SWIG_INTERFACE_MDX_CONTENT}")
@@ -38,11 +99,15 @@ MACRO(END_WRAP_LIBRARY_SWIG_INTERFACE)
      ${includes_file}
      @ONLY IMMEDIATE )
 
-  ADD_CUSTOM_TARGET(${WRAPPER_LIBRARY_NAME}Idx DEPENDS ${SWIG_INTERFACE_IDX_FILES})
-  ADD_DEPENDENCIES(${WRAPPER_LIBRARY_NAME}Swig ${WRAPPER_LIBRARY_NAME}Idx)
+#   ADD_CUSTOM_TARGET(${WRAPPER_LIBRARY_NAME}Idx DEPENDS ${SWIG_INTERFACE_IDX_FILES})
+  SET(${WRAPPER_LIBRARY_NAME}IdxFiles ${SWIG_INTERFACE_IDX_FILES} CACHE INTERNAL "Internal ${WRAPPER_LIBRARY_NAME}Idx file list.")
+
+  ADD_CUSTOM_TARGET(${WRAPPER_LIBRARY_NAME}Swig DEPENDS ${SWIG_INTERFACE_FILES})
+  SET(${WRAPPER_LIBRARY_NAME}SwigFiles ${SWIG_INTERFACE_FILES} CACHE INTERNAL "Internal ${WRAPPER_LIBRARY_NAME}Swig file list.")
   FOREACH(dep ${WRAPPER_LIBRARY_DEPENDS})
-    ADD_DEPENDENCIES(${WRAPPER_LIBRARY_NAME}Swig ${dep}Idx ${dep}Swig)
+    ADD_DEPENDENCIES(${WRAPPER_LIBRARY_NAME}Swig ${dep}Swig) # ${dep}Idx
   ENDFOREACH(dep)
+
 ENDMACRO(END_WRAP_LIBRARY_SWIG_INTERFACE)
 
 
@@ -80,65 +145,15 @@ MACRO(END_INCLUDE_WRAP_CMAKE_SWIG_INTERFACE module)
           ${xml_file} ${idx_file}
     DEPENDS ${CABLE_INDEX} ${xml_file}
   )
-  ADD_CUSTOM_TARGET(${module}Idx DEPENDS ${idx_file})
+#   ADD_CUSTOM_TARGET(${module}Idx DEPENDS ${idx_file})
 
   # store the path of the idx file to store it in the mdx file
   SET(SWIG_INTERFACE_MDX_CONTENT "${SWIG_INTERFACE_MDX_CONTENT}${idx_file}\n")
   SET(SWIG_INTERFACE_IDX_FILES ${SWIG_INTERFACE_IDX_FILES} ${idx_file})
   
-  # create the swig interface
-  SET(interface_file "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/wrap_${module}.i")
-  SET(xml_file "${WRAPPER_LIBRARY_OUTPUT_DIR}/wrap_${module}.xml")
-  SET(includes_file "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${WRAPPER_LIBRARY_NAME}.includes")
-  # prepare the options
-  SET(opts )
-  FOREACH(dep ${WRAPPER_LIBRARY_DEPENDS})
-    SET(opts ${opts} --mdx "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${dep}.mdx")
-    SET(opts ${opts} --take-includes "${WRAPPER_MASTER_INDEX_OUTPUT_DIR}/${dep}.includes")
-    SET(opts ${opts} --import "${dep}.i")
-  ENDFOREACH(dep)
-  # import the interface files previously defined instead of importing all the files defined
-  # in the module to avoid many warnings when running swig
-  FOREACH(i ${SWIG_INTERFACE_FILES})
-    GET_FILENAME_COMPONENT(bi "${i}" NAME)
-    SET(opts ${opts} --import "${bi}")
-  ENDFOREACH(i)
-
-# MESSAGE("${opts}")
-
-  # configure the test driver, to set the python path to the pygccxml dir
-  FIND_PROGRAM(ITK_TEST_DRIVER itkTestDriver)
-  SET(PYTHON_PYGCCXML_DRIVER "${ITK_TEST_DRIVER}"
-    --add-before-env PYTHONPATH "${WRAP_ITK_CMAKE_DIR}/pygccxml-0.8.2/"
-    "${PYTHON_EXECUTABLE}"
-  )
-  ADD_CUSTOM_COMMAND(
-    OUTPUT ${interface_file}
-    COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_BINARY_DIR}/igenerator.py
-      ${opts}
-      --mdx ${mdx_file}
-      --take-includes ${includes_file}
-#       --import ${module_interface_file}
-      --swig-include wrap_${module}_ext.i
-      -w1 -w3 -w51 -w52 -w53 -w54 #--warning-error
-      # --verbose
-      ${xml_file}
-      ${interface_file}
-    DEPENDS ${xml_file} ${idx_file} ${includes_file} ${CMAKE_BINARY_DIR}/igenerator.py ${SWIG_INTERFACE_FILES}
-  )
-  ADD_CUSTOM_TARGET(${module}Swig DEPENDS ${interface_file})
-  ADD_DEPENDENCIES(${module}Swig ${WRAPPER_LIBRARY_NAME}Idx)
-  ADD_DEPENDENCIES(${WRAPPER_LIBRARY_NAME}Swig ${module}Swig)
-  FOREACH(dep ${WRAPPER_LIBRARY_DEPENDS})
-    ADD_DEPENDENCIES(${module}Swig ${dep}Idx ${dep}Swig)
-    # be sure that the module is selected by the user
-    IF(NOT "${WRAP_ITK_MODULES}" MATCHES "(^|;)${dep}(;|$)")
-      MESSAGE(SEND_ERROR "${dep} is required by ${WRAPPER_LIBRARY_NAME} module. Please set WRAP_${dep} to ON, or WRAP_${WRAPPER_LIBRARY_NAME} to OFF.")
-    ENDIF(NOT "${WRAP_ITK_MODULES}" MATCHES "(^|;)${dep}(;|$)")
-  ENDFOREACH(dep)
-
-  SET(SWIG_INTERFACE_FILES ${SWIG_INTERFACE_FILES} ${interface_file})
   SET(SWIG_INTERFACE_MODULE_CONTENT "${SWIG_INTERFACE_MODULE_CONTENT}%import wrap_${module}.i\n")
+
+  SET(SWIG_INTERFACE_MODULES ${SWIG_INTERFACE_MODULES} ${module})
 ENDMACRO(END_INCLUDE_WRAP_CMAKE_SWIG_INTERFACE)
 
 
